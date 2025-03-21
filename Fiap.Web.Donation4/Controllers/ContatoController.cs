e237f35a6eba2534753dbaff5e5f33e29ccba657
Flavio Moreni <PF0954@fiap.com.br>
2025-03-20 22:01:18 -0300

    Add project files.

---
+++ Fiap.Web.Donation4/Controllers/ContatoController.cs
@@ -0,0 +1,42 @@
+﻿using Fiap.Web.Donation4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.Donation4.Controllers
+{
+    public class ContatoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            Console.WriteLine("Contato -- Index");
+
+            //"SELECT nomeMensagem FROM TBMENSAGEM WHERE ..."
+
+            //return RedirectToAction("Index","Home");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(ContatoModel contatoModel)
+        {
+            // INSERT INTO TBCONTATO VALUES (contatoModel.Nome, contatoModel.Sobrenome ...
+
+            Console.WriteLine("Contato -- Cadastrar");
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Help()
+        {
+            Console.WriteLine("Contato -- Help");
+
+            var mensagem = "Carregada do banco de dados";
+            TempData["msg"] = mensagem;
+
+
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ Fiap.Web.Donation4/Models/ContatoModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Fiap.Web.Donation4.Models
+{
+    public class ContatoModel
+    {
+        public int ContatoId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Telefone { get; set; }
+
+        public string Mensagem { get; set; }
+
+    }
+}
